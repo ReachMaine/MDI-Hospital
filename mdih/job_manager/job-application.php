e3040db137033ad033d91a3655ad05357c1f2db8
Linda Speight <linda@reachmaine.com>
2016-10-21 09:51:02 -0400

    hiding apply not button

--- mdih/job_manager/job-application.php
+++ mdih/job_manager/job-application.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $apply = get_the_job_application_method();
 if (!$apply ) {
 	echo "<!-- apply is blank. -->";
@@ -9,16 +9,20 @@ if (!$apply ) {
 <?php }
 if ( $apply ) : ?>
 	<?php if ( 'url' === $apply->type ) : ?>
+		<?php if ($apply->url == "http://false") {
+
+		} else { ?>
 		<div class="job_application application">
 			<a href="<?php echo esc_url( $apply->url ); ?>" class="application_button button cws_button medium" target="_blank" rel="nofollow">Apply Now</a>
 		</div>
+	<?php } ?>
 	<?php else : ?>
 		<?php wp_enqueue_script( 'wp-job-manager-job-application' ); ?>
 		<div class="job_application application">
 			<?php do_action( 'job_application_start', $apply ); ?>
-		
+
 			<input type="button" class="application_button button" value="<?php _e( 'Apply for job', 'wp-job-manager' ); ?>" />
-		
+
 			<div class="application_details">
 				<?php
 				do_action( 'job_manager_application_details_' . $apply->type, $apply );
@@ -27,4 +31,4 @@ if ( $apply ) : ?>
 			<?php do_action( 'job_application_end', $apply ); ?>
 		</div>
 	<?php endif; ?>
-<?php endif; ?>	
+<?php endif; ?>
