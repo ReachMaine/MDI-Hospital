a51294cbb23acd859914aa44f3ca0f2e27c4a5eb
Linda "zig" Speight <linda@reachmaine.com>
2017-06-12 15:46:33 -0400

    change of strategy on team-members
    use button to single team-member instead of expansion.

--- mdih/custom/ourteam.php
+++ mdih/custom/ourteam.php
@@ -6,9 +6,14 @@ add_filter('woothemes_our_team_content', 'reach_filter_content', 9, 2);
 function reach_filter_content($in_content, $post) {
   $out_content = "";
   if ($in_content) {
-      $out_content .= '[cws-widget type=accs title="" toggle=1  items=1][item type=accs title="More Info"]';
+    if ($in_content) {
+    $target = get_the_permalink($post);
+    $out_content .= '<a class="cws_button small" href="'.$target.'">See More </a>';
+        /* $out_content .= '[cws-widget type=accs title="" toggle=1  items=1][item type=accs title="See More"]';
       $out_content .= $in_content;
       $out_content .= '[/item][/cws-widget]';
+      $out_content .= do_shortcode($out_content); */
+}
       //$out_content .= do_shortcode($out_content);
   }
   return $out_content;
