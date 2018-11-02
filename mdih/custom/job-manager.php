5be0dd1d856e0b816f4b3d8966892a0379fe1774
linda@reachmaine.com <linda@reachmaine.com>
2018-11-02 14:09:07 -0400

    add blurb to display of each single job listing
    competitive salary,  EOE, etc.

--- mdih/custom/job-manager.php
+++ mdih/custom/job-manager.php
@@ -10,4 +10,14 @@
 		return $query_args;
 	}
 
-add_filter( 'job_manager_get_listings', 'modify_wpjm_query_args' ); 
+add_filter( 'job_manager_get_listings', 'modify_wpjm_query_args' );
+
+// use action to add text at bottom of every job listings
+// so ronda doesnt have to copy & paste it every time.
+add_action('single_job_listing_end', 'mdih_add_company_blur_to_bottom'); // this puts it after the apply now.
+function mdih_add_company_blur_to_bottom () {
+	echo '<div class="mdih-career-blurb">';
+	echo 		'<p>MDI Hospital offers a competitive salary, medical/dental/life insurance, matching retirement plan, paid vacation and sick time, wellness program, tuition reimbursement, and continuing education benefits. </p>';
+	echo    '<p>Equal Opportunity Employer/Protected Veterans/Individuals with Disabilities.</p>';
+	echo '</div>';
+}
