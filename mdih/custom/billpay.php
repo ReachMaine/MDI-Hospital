2f9ae1071f3e8da10d85b00982b34005d23778b2
Linda "zig" Speight <linda@reachmaine.com>
2023-02-01 13:13:25 -0500

    update bill pay

--- mdih/custom/billpay.php
+++ mdih/custom/billpay.php
@@ -6,9 +6,10 @@ function mdih_billpay_form() {
 
 	     $form = '<form class="mdih-billpay-form" action="https://hosted.transactionexpress.com/Transaction/Transaction/Index" method="POST" target="_blank" oninput="CustRefID.value = CustName.value.toUpperCase() + 	&#39;/INV#&#39; + CustInvoice.value">';
 				//$form .= '<form class="mdih-billpay-form" action="https://hosted.transactionexpress.com/Transaction/Transaction/Index" method="POST" target="_blank">';
-				$form .= '<input id="HostedKey" name="HostedKey" type="hidden" value="f1dc2cf8-a28b-47ce-8133-d379430bc096" />';
-				$form .= '<input id="Gateway_ID" name="Gateway_ID" type="hidden" value="9007813296" /><input id="IndustryCode" name="IndustryCode" type="hidden" value="2" />';
-	      $form .= '<input id="RecurringType" name="RecurringType" type="hidden" value="N" />';
+				$form .= '<input type="hidden" name="HostedKey" id="HostedKey" value="9b11f7c1-8b0b-4e2f-9e35-3f434f49901d" />';
+				$form .= '<input type="hidden" name="Gateway_ID" id="Gateway_ID" value="9040658319" />';
+				$form .= '<input id="IndustryCode" name="IndustryCode" type="hidden" value="2" />';
+	    		$form .= '<input id="RecurringType" name="RecurringType" type="hidden" value="N" />';
 				$form .= '<input id="RecurringAmount" name="RecurringAmount" type="hidden" value="" />';
 				$form .= '<input id="RURL" name="RURL" type="hidden" value="https://www.mdihospital.org/thanks/" />';
 				$form .= '<input id="CURL" name="CURL" type="hidden" value="https://www.mdihospital.org/insurance-and-billing/" />';
@@ -18,11 +19,11 @@ function mdih_billpay_form() {
 				$form .= '<input id="Descriptor" name="Descriptor" type="hidden" value="Doctor" />';
 				$form .= '<input name="CustRefID" type="hidden" />';
 				// visible form fields
-	      $form .= '<div class="mdih-billpay-field"><label>Patient Name:</label><input class="paynow" maxlength="25" name="CustName" required="" type="text" value="" /></div>';
+	      		$form .= '<div class="mdih-billpay-field"><label>Patient Name:</label><input class="paynow" maxlength="25" name="CustName" required="" type="text" value="" /></div>';
 				$form .= '<div class="mdih-billpay-field"><label>Account Number:</label><input class="paynow" maxlength="15" name="CustInvoice" required="" type="text" value="" /></div>';
 				$form .= '<div class="mdih-billpay-field"><label>Your Email: </label><input id="Email" name="Email" required="" type="text" value="" placeholder="your@email.com" /></div>';
 				$form .= '<div class="mdih-billpay-field"><label>Phone number:</label><input id="PhoneNumber" name="PhoneNumber" required="" type="text" value="" placeholder="123-123-1234" pattern = "^\d{3}-\d{3}-\d{4}$" />';
-					$form .= '<div class="mdih-billpay-instr">Please use phone number with dashes like 207-288-5081.</div>';
+				$form .= '<div class="mdih-billpay-instr">Please use phone number with dashes like 207-288-5081.</div>';
 				$form .= '</div>';
 				$form .= '<div class="mdih-billpay-field"><label>Amount:</label> <input id="Amount" name="Amount" type="Text" value="" required=""
 pattern = "^\d{1,6}\.\d{2}?$"/>';
